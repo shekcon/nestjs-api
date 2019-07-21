989dfd565cae27a83a9cbea40d32bb5e5a29033d
Shekcon <quangsang9773@gmail.com>
2019-07-22 06:01:14 +0700

    + Fix typeorm error unexpected token + Add config to deploy heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,21 @@ import { AppService } from "./app.service";
 import { UsersModule } from "./modules/users/users.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import * as path from "path";
+import { User } from "./modules/users/users.entity";
+
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
     TypeOrmModule.forRoot({
-      entities: [path.join(__dirname, "**/*.entity{.ts,.js}")]
-    })
+      type: "postgres",
+      url: process.env.DATABASE_URL || "postgres://postgres:123456@localhost:5432/nestjs_api",
+      synchronize: true,
+      entities: [User],
+  })
   ],
   controllers: [AppController],
   providers: [AppService]
 })
-export class AppModule {}
+export class AppModule { }
